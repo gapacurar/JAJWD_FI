0aea44677655fb291325c3d65d2c76c58e1a6ca3
Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>
2019-05-08 16:14:18 +0300

    Creare prima versiune <gheor@SURFACE-PRO4-AP> Signed-off-by:Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>

---
+++ src/myfirstproject/MyFirstProject.java
@@ -0,0 +1,26 @@
+/*
+ * This is my first java class and program
+ */
+
+// every class file should be created into a package, defaultpackage will be
+// used instead.
+package myfirstproject;
+
+/**
+ *
+ * @author gheorghe aurel pacurar
+ */
+public class MyFirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Daily program
+        System.out.println("Ma trezesc!");
+        System.out.println("Ma spal!");
+        System.out.println("Ma imbrac!");
+        System.out.println("Mananc micul dejun!");
+        System.out.println("Merg la job daca este zi lucratoare.");
+    }
+}
